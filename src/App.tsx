11b43f9ac6c743d1856b74b03ee386468a834751
Min Hui Tan <minhuitan@Mins-MacBook-Air.local>
2025-04-27 23:01:28 +0800

    added Auth UI & removed todos

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,6 @@ import { Authenticator } from '@aws-amplify/ui-react';
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
 
   return (
     <Authenticator>
